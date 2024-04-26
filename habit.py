f1e8e568a9b096eccb9d4751941bc1a6dfb1ef02
Linaly Miyamoto <41589881+linishere@users.noreply.github.com>
2024-04-26 11:26:17 -0400

    added docstrings
    adding some docstrings

--- habit.py
+++ habit.py
@@ -4,16 +4,38 @@ import argparse
 import sys
 
 class User:
+    """ This is the class for the user 
+
+    Attributes: 
+        user_id: This is the user id 
+        username (str): this is the username for the user
+        password (str): the password of the user
+    """
     def __init__(self, user_id, username, password):
-        self.user_id = user_id
+        self.user_id = user_id 
         self.username = username
         self.password = password
         
 class Manager:
+    """ This is the class for managing the users and storing them in a 
+        dictionary
+    
+        Attributes: 
+            users: this is for storing the users
+    """
     def __init__(self):
         self.users = {}
         
 class Habit:
+    """ This represents a habit
+    
+        Attributes: 
+            user_id: the user id of the user 
+            name: 
+            category: 
+            frequency: 
+            streak: 
+    """
     def __init__(self, user_id, name, category, frequency, streak: str=None, database=" "):
         self.user_id = user_id
         self.name = name
@@ -22,6 +44,8 @@ class Habit:
         self.streak = 0
         
 class Tracker:
+    """ It manages habits and stores them 
+    """
     def __init__(self):
         self.habits = {}
         
@@ -30,6 +54,8 @@ class Tracker:
 
 
 def parse_args(args_list):
+    """
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, help='Path to the text file')
     return parser.parse_args(args_list)
