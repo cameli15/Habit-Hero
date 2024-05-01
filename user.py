e62d2dd0222833d85f839e8e73bd0d133c460a17
cameli15 <163566097+cameli15@users.noreply.github.com>
2024-04-30 23:43:07 -0400

    Update user.py

--- user.py
+++ user.py
@@ -34,10 +34,37 @@ class User:
         self.username = username
         self.password = password
 
-        #db connection should go here
+        db_path = Path(__file__).parent / "main_db.db"
+        self.conn = sqlite3.connect(db_path)
+        self.cur = self.conn.cursor()
+        
 #store data into database
     def store_in_db(self):
       """Stores user data into DB when registering"""
+        
+    # store habit into database
+    def store_habit_in_db(self, new_habit):
+
+# function to retrieve habit from the db
+    def retrieve_habit(self, habit_name):
+
+# maybe we can have present choices for users to choose from so we can demonstrate the program, we don't need it if you guys dont think its neccesary
+    def choose_preset_habit(self):
+
+     # where user can create a habit
+    def create_habit(self):
+
+# where user can delete a habit out of the database, again idk if we'll 100% need this but just as an option
+    def delete_habit(self):
+
+# function that prints out all of the data within the database so users can see their progress
+    def show_all(self):
+
+# shows user current streak overview of all their habits stored by frequency
+    def current_streak_overview(self):
+
+
+
 
         
     
