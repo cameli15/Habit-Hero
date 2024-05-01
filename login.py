e267b87c389623c143821e381c5ade6d144c0086
cameli15 <163566097+cameli15@users.noreply.github.com>
2024-04-30 23:51:28 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,9 +1,16 @@
+# this is where we will create and launch the database
 def launch_database():
 
+# where users can register, there will be a set of questions within this method
 def register_user():
 
+
+# if its not a users first time they'll be redirected to this that will retrieve their existing data 
 def get_user():
 
+# this is where users can login and insert their information
 def login():
 
+
+# this can be like pw validation? not sure if we'll need it but if they're using a password maybe we should have a response instead of an error?
 def check_password():
