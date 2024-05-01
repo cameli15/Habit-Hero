f390bbfe278127615506bd8881df1c3230aa23c4
cameli15 <163566097+cameli15@users.noreply.github.com>
2024-04-30 20:01:01 -0400

    Update user.py

--- user.py
+++ user.py
@@ -1,1 +1,43 @@
+# this file will contain our User Class that will be where users can track their habits, create new ones, delete, and possibly choose from pre-exisiting ones
 
+import sqlite3
+import habit
+from PyInquirer import prompt
+
+class User:
+    """
+    A user class to represent a user of HabitHero.
+    
+    Attributes: 
+        firstname (str) - first name of user
+        lastname (str) - last name of user
+        username (str) - username of user
+        password (str) - password of user
+        
+    Methods:
+
+    """
+    # Init method
+    def __init__(self, firstname, lastname, username, password):
+        """
+        Initializes User class and sets up the attributes for the user.
+        
+        Parameters:
+            self (str)
+            firstname (str) - first name of user
+            lastname (str) - last name of user
+            username (str) - username of user
+            password (str) - password of user
+        """
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = password
+
+        #db connection should go here
+#store data into database
+    def store_in_db(self):
+      """Stores user data into DB when registering"""
+
+        
+    
