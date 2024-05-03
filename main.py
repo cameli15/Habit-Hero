e0a94c3953141181d965b5e0570bf08f9f46bb65
lakshyasajal <147116458+lakshyasajal@users.noreply.github.com>
2024-05-03 09:39:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,99 @@
-#install PyInquirer using pip install PyInquirer in command line
-#login.py is another file that will be our main file for the program
-
-from PyInquirer import prompt
+import questionary
 import login
-import user
+import database_manager
 
-# creates database
-login.launch_database()
 
-# program starts and we'll implement an opening message
-welcome_message = "\nHello! Welcome to Habit Hero!\n" 
-
-print(welcome_message)
-
-# this is where users will login/register using PyInquirer
-
-questions = [
-    {
-        'type': 'list',
-        'name': 'action',
-        'message': 'Is this your first time using HabitHero or are you a returning user?',
-        'choices': [
-            'Register',
-            'Login'
-        ]
-    }
-]
-
-answer = prompt(questions)
-
-if answer['action'] == 'Login':
-    user = login.login()
-    print("We're happy to see you back!\n")
-elif answer['action'] == 'Register':
-    print("\nWelcome! Since it's your first time here, let's get you set up.\n")
-    login.register_user()
-    print("\nPlease log in\n")
-    user = login.login()
+def program_menu():
+    """docstring here"""
+    database_manager.launch_database()
     
-# if its a newly registered user, insert choices from habits or allow them to create?
-# whatever is best for the project
-
-#create menu for the beginning of the program
-def menu():
-    """
-    Main menu of our program.
+    print("\nHello! Welcome to Habit Hero!\n")
+    initial_message = questionary.select(
+        "Is this your first time here?", choices=[
+            "Yes! First time!",
+            "Nope! :D"
+        ]).ask()
     
-    Asks user what they want to do from a list of choices.
-    """
-    menu_choices = [
-        {
-            'type': 'list',
-            'name': 'action',
-            'message': 'What do you want to do?',
-            'choices': [
-                'Create, Delete or Mark a Habit as completed',
-                'Activity Overview',
-                'View Habits',
-                Separator(),
-                'Exit Program'
-            ]
-        }
-    ]
+    if initial_message == "Yes! First time!":
+        action = questionary.select(
+            "Welcome, Let's get you started with your registration!",
+            choices=[
+                "Register! :D",
+                "Exit :("
+            ]).ask()
+        
+        if action == "Register! :D":
+            user = login.register_user()  
+            if user:
+                print(f"\nWelcome!\n")
+                return user
+            else:
+                return None
+        elif action == "Exit :(":
+            print("Exiting the program.")
+            return None
+    
+    elif initial_message == "Nope! :D":
+        action = questionary.select(
+            "Welcome back! What would you like to do?",
+            choices=[
+                "Login",
+                "Exit :("
+            ]).ask()
+        if action == "Login":
+            user = login.login()
+            if user:
+                print(f"\nWelcome back, {user.firstname}!\n")
+                return user
+        elif action == "Exit :(":
+            print("Exiting the program.")
+            return None
 
-    second_question = prompt(menu_choices)['action']
 
-    if second_question == 'Create, Delete or Mark a Habit as completed':
-        habit_choices = [
-            {
-                'type': 'list',
-                'name': 'habit_action',
-                'message': 'Do you want to:',
-                'choices': [
-                    'Create a new habit',
-                    'Delete habit',
-                    'Mark a habit as completed'
-                ]
-            }
-        ]
-        habit_question = prompt(habit_choices)['habit_action']
+def main():
+    """docstring here"""
+    try:
+        user = program_menu()
+        if not user:
+            return  # User chose to exit or failed to login/register
 
-        if habit_question == 'Create a new habit':
-            print("Let's create a new habit.\n")
-            new_habit = user.create_habit()
-            user.store_habit_in_db(new_habit)
-            print("\nWhat do you want to do next?\n")
-            menu()
-
-        elif habit_question == 'Delete habit':
-            user.delete_habit()
-            print("\nWhat do you want to do next?\n")
-            menu()
-        elif habit_question == 'Mark a habit as completed':
-            user.is_completed()  
-            print("\nWhat do you want to do next?\n")
-            menu()
-
-        elif second_question == 'Exit Program':
-            print(f"\nSee you soon, {user.firstname}!\n")   
+        while True:
+            menu_question = questionary.select("What would you like to do?",
+                                               choices=[
+                                                   "Add, Delete, or Mark a Habit as complete",
+                                                   "View Habits",
+                                                   "Exit Habit Hero"
+                                               ]).ask()
+            if menu_question == "View Habits":
+                print("Let's take a look!\n")
+                user.show_all()
+        
+            elif menu_question == "Add, Delete, or Mark a Habit as complete":
+                habit_choice = questionary.select("What would you like to do?",
+                                            choices=[
+                                                "Add Habit",
+                                                "Delete Habit",
+                                                "Mark a habit as completed! Yay!"
+                                            ]).ask()
+        
+                if habit_choice == "Add Habit":
+                    print("Great! Let's get a new habit started!")
+                    new_habit = user.add_habit()
+                    user.store_habit_in_db(new_habit)
+                
+                elif habit_choice == "Delete Habit":
+                    user.delete_habit()
+                
+                elif habit_choice == "Mark a habit as completed! Yay!":
+                    user.habit_completed()
+        
+            elif menu_question == "Exit Habit Hero":
+                print(f"\nBye {user.firstname}! We hope you come back soon!\n")
+                break  
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+            
+            
+if __name__ == "__main__":
+    main()
